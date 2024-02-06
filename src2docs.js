47b975ad27d45c9e5248e656a1c8df9ea1af82e5
gevaland <48095159+gevaland@users.noreply.github.com>
2024-02-06 22:20:18 +0000

    Fixed containers gen

--- src2docs.js
+++ src2docs.js
@@ -3,6 +3,6 @@ import { run } from "@mermaid-js/mermaid-cli"
 await run("src/uml-usecases.mmd", "docs/uml-usecases.svg")
 await run("src/sysml-requirements.mmd", "docs/sysml-requirements.svg")
 await run("src/c4-context.mmd", "docs/c4-context.svg")
-await run("src/c4-context.mmd", "docs/c4-containers.svg")
+await run("src/c4-containers.mmd", "docs/c4-containers.svg")
 await run("src/c4-components.mmd", "docs/c4-components.svg")
 await run("src/readme.template.md", "docs/README.md")
